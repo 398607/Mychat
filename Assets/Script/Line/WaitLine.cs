c38e612c28c199ebf7e814e280cae36181f04e2f
nagizero <nagizero@foxmail.com>
2016-01-14 20:42:10 +0800

    WaitLine.cs

---
+++ Assets/Script/Line/WaitLine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaitLine : Line
+{
+	public int WaitIndex;
+	public int WaitValue;
+
+	public WaitLine(int waitIndex, int waitValue)
+	{
+		WaitIndex = waitIndex;
+		WaitValue = waitValue;
+	}
+
+	public override bool React()
+	{
+		while (!GameManager.FitAsked(WaitIndex, WaitValue))
+		{
+			Wait();
+		}
+		return true;
+	}
+
+	private IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(1f);
+	}
+}
